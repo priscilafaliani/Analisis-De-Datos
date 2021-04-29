d62d4d158b9a38bc8320bc6e374d9910105dd433
Priscila Elizabeth Faliani <priscilafaliani@gmail.com>
2021-04-29 06:03:43 -0300

    Agrega archivo encargado de ejecutar el programa.

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-import src.windows
+from src.windows import start_window
+from src.windows import popup
+import src.globals as g
+
 
 start_window.build().read(close=True)
-
+popup.build(g.ELI5_CARD_TITLE, g.ELI5_CARD_SUBTITLE, g.ELI5_CONTENT).read(close=True)
+popup.build(g.LPT_CARD_TITLE, g.LPT_CARD_SUBTITLE, g.LPT_CONTENT).read(close=True)
