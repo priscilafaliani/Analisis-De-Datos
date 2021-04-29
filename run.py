8810f7da950369bb3573a22676c82ed37bdd3700
Priscila Faliani <priscilafaliani@gmail.com>
2021-04-29 16:30:53 -0300

    Script de ejecución llama las funciones correctas.

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
-from src.windows import menu
-from src.windows import popup
-import src.globals as g
+from src import main
 
-
-menu.build().read(close=True)
-popup.build(g.TIL_CARD_TITLE, g.TIL_CARD_SUBTITLE, g.TIL_CONTENT).read(close=True)
-popup.build(g.LPT_CARD_TITLE, g.LPT_CARD_SUBTITLE, g.LPT_CONTENT).read(close=True)
+if __name__ == '__main__':
+    main.start()
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from src.components import menu
+
+def start():
+    menu.start()
